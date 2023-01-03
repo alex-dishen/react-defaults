80754f3a0a3876f58969ac0c9209397a816fde89
Alex <didyshen.oleksandr@gmail.com>
2023-01-03 10:06:57 +0200

    Change App wrapper

--- src/index.js
+++ src/index.js
@@ -6,8 +6,7 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <Provider store={store} />
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
